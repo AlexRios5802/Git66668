95bf9e005a11e1163bcbc3f06904438ee2142a6c
USUARIOWIN <USUARIOWIN@Alex-Rios>
2021-02-18 20:20:06 -0600

    Estoy igualando mis dos copias, el working copy y el local repository

---
+++ src/git66668/Git66668.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git66668;
+
+/**
+ *
+ * @author USUARIOWIN
+ */
+public class Git66668 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Alex Rios";
+        String matricula = "66668";
+        System.out.println("Soy, " + nombre + "  y mi matricula es " +matricula);
+        //Repositorio de mi programa xd
+        int edad = 19;
+    }
+    
+}
